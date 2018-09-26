49cd5ad9e949257868354e3c1642a4f58f2364b0
yimoxiamu <2959623954@qq.com>
2018-09-26 10:53:23 +0800

    xiamu

---
+++ src/main/java/com/yimoxiamu/blogback/BlogBackApplication.java
@@ -0,0 +1,15 @@
+package com.yimoxiamu.blogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiamu
+ */
+@SpringBootApplication
+public class BlogBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBackApplication.class, args);
+    }
+}
